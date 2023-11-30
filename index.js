9d52c71fb4ea85d4ad9a8c81f472c7c0a9acfa1c
Your Name <abirdutta90990@gmail.com>
2023-11-30 21:45:46 +0530

    Server side fetching

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const cors=require("cors");
+const cors = require("cors");
 require('dotenv').config()
 const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
-const {run}=require("./scrapeLogic");
+const { run } = require("./scrapeLogic");
 const PORT = process.env.PORT || 4000;
 app.use(cors());
 const cron = require('node-cron');
@@ -14,12 +14,21 @@ const { scrapeNewsAndSave } = require("./CronJobs/cronScripts");
 mongoose.connect(process.env.MONGO_PASS, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get("/scrape/:link", (req, res) => {
-    const link=req.params.link;
- run(res, link);
+  const link = req.params.link;
+  run(res, link);
 });
 
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
+});
+app.get("/test", (req, res) => {
+  try {
+    const response = fetch("https://www.imdb.com/");
+    res.send(response);
+
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 
